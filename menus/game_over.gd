65c9efe407be10bac0d052297627cefa00d66fdd
Apostolos Karamanis <apostkaram@gmail.com>
2025-05-14 12:39:31 +0300

    highscore save

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,14 +1,19 @@
 extends Control
 
+const SAVE_PATH =  "user://save.cfg"
 
 @export var game_stats: GameStats
+
+var save_path = SAVE_PATH
 
 @onready var score_value: Label = %ScoreValue
 @onready var high_score_value: Label = %HighScoreValue
 
 func _ready() -> void:
+	load_highscore()
 	if game_stats.score > game_stats.highscore:
 		game_stats.highscore = game_stats.score
+		save_highscore()
 	score_value.text = str(game_stats.score)
 	high_score_value.text  = str(game_stats.highscore)
 
@@ -18,3 +23,14 @@ func _process(delta: float) -> void:
 		TransitionScreen.transition()
 		await get_tree().create_timer(1).timeout
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
+
+func load_highscore() -> void:
+	var config = ConfigFile.new()
+	var error = config.load(save_path)
+	if error != OK: return
+	game_stats.highscore = config.get_value("game", "highscore")
+	
+func save_highscore() -> void:
+	var config = ConfigFile.new()
+	config.set_value("game", "highscore", game_stats.highscore)
+	config.save(save_path)
